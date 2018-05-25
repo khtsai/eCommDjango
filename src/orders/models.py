71499febf5a2cb1ffeea18f88ce1644e1bf6c68b
WINCTRL-GFST2VR\Chuck <echo770719@gmail.com>
2018-05-25 09:49:07 +0200

    Add random order id and

--- src/orders/models.py
+++ src/orders/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.db.models.signals import pre_save
+from .utils import unique_order_id_generator
 from carts.models import Cart
 
 ORDER_STATUS_CHOICES = {
@@ -22,3 +23,8 @@ class Order(models.Model):
 	def __str__(self):
 		return self.order_id
 
+def pre_save_create_order_id(sender, instance, *args, **kwargs):
+	if not instance.order_id:
+		instance.order_id = unique_order_id_generator(instance)
+
+pre_save.connect(pre_save_create_order_id, sender = Order)
