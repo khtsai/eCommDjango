454ef30c9874dce6b77d7505071dbe0051fc9889
WINCTRL-GFST2VR\Chuck <echo770719@gmail.com>
2018-06-07 17:10:14 +0200

    Add billing model and combine checkout with login

--- src/carts/views.py
+++ src/carts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from orders.models import Order
 from .models import Cart
 from products.models import Product
+from accounts.forms import LoginForm
+from billing.models import BillingProfile
 # Create your views here.
 
 def cart_create():
@@ -50,7 +52,20 @@ def checkout_home(request):
         return redirect("cart:home")
     else:
         order_obj, new_order_obj = Order.objects.get_or_create(cart=cart_obj)
-    return render(request, "carts/checkout.html", {"object":order_obj})
+
+    user = request.user
+    billing_profile = None
+    login_form = LoginForm()
+
+    if user.is_authenticated():
+        billing_profile, billing_profile_created = BillingProfile.objects.get_or_create(user=user, email=user.email)
+    context = {
+        "object": order_obj,
+        "billing_profile": billing_profile,
+        "login_form": login_form
+    }
+
+    return render(request, "carts/checkout.html", context)
 
 
 
